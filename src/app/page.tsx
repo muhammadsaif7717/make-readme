d6b0822bb74659814a352ac64e879bf79f19d197
MD. Saif Islam <muhammadsaif7717@gmail.com>
2026-02-06 00:41:41 +0600

    added hero & theme toggler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/root/Hero";
 
 
 export default function Main() {
   return (
     <div>
-     
+     <Hero/>
     </div>
   )
 }
---
+++ src/providers/NextThemeProvider.tsx
@@ -0,0 +1,16 @@
+// components/NextThemeProvider.tsx
+'use client'
+
+import { ThemeProvider } from "next-themes";
+
+export default function NextThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider 
+      attribute="class" 
+      enableSystem 
+      defaultTheme="light"
+    >
+      {children}
+    </ThemeProvider>
+  )
+}
