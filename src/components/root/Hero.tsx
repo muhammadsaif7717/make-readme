f6972347fa629fdb23bc2553244fbb074d76c2ae
MD. Saif Islam <muhammadsaif7717@gmail.com>
2026-02-06 10:00:51 +0600

    Added all fields

--- src/components/root/Hero.tsx
+++ src/components/root/Hero.tsx
@@ -1,16 +1,21 @@
-import Link from "next/link";
-import { Button } from "../ui/button";
-
+import Link from 'next/link';
+import { Button } from '../ui/button';
 
 export default function Hero() {
-    return (
-        <div className='h-screen flex flex-col text-center justify-center items-center'>
-            <div className="space-y-7">
-                <h1 className='text-5xl font-semibold'>The Easiest Way To Create A</h1>
-                <h1 className='text-5xl font-semibold text-green-500 uppercase'>Readme</h1>
-               <Link href={'/make-readme'}> <Button className="bg-green-500 px-8 py-6 rounded-full w-56 mx-auto  hover:bg-green-600 text-2xl">Get Started</Button></Link>
-            </div>
-
-        </div>
-    )
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-red-500 text-center">
+      <div className="space-y-7">
+        <h1 className="text-5xl font-semibold">The Easiest Way To Create A</h1>
+        <h1 className="text-5xl font-semibold text-green-500 uppercase">
+          Readme
+        </h1>
+        <Link href={'/make-readme'}>
+          {' '}
+          <Button className="mx-auto w-56 rounded-full bg-green-500 px-8 py-6 text-2xl hover:bg-green-600">
+            Get Started
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
